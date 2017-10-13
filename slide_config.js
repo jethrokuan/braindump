01ab8f0d622a59078137c704bad1871f204b1cfe
Jethro Kuan <jethrokuan95@gmail.com>
2017-10-13 16:52:31 +0800

    add slide config

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Docker 101', 
+     subtitle: 'Hackerschool, NUS Hackers', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/docker/docker.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Jethro Kuan', 
+     company: '', 
+     gplus: '', 
+     twitter: '@nushackers', 
+     www: 'http://www.nushackers.org/', 
+     github: '', 
+   }]
+};
