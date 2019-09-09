eaa63c3d17b3c761a20230db9d5b71d8309f75c6
Jethro Kuan <jethrokuan95@gmail.com>
2019-09-09 15:47:09 +0800

    home/jethro/.org/braindump/.dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
-(("org/"
-  . ((org-mode . ((eval . (org-hugo-auto-export-mode)))))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+(("org/" . ((org-mode . ((eval org-hugo-auto-export-mode))))))
