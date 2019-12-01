37deb934e4f9bb939c970dd6fe4b1b154f0c905a
Jethro Kuan <jethrokuan95@gmail.com>
2019-12-01 16:39:20 +0800

    insert boilerplate on new zettel

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-(("org/" . ((org-mode . ((eval org-hugo-auto-export-mode))))))
+(("org/" . ((org-mode . ((eval org-hugo-auto-export-mode)
+                         (eval jethro/deft-insert-boilerplate))))))
