16097a9f9a49d462af112d800d417bd23c65e028
Jethro Kuan <jethrokuan95@gmail.com>
2020-01-22 19:37:22 +0800

    Wed 22 Jan 2020 07:37:24 PM +08

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-(("org/" . ((org-mode . ((eval org-hugo-auto-export-mode)
-                         (eval jethro/deft-insert-boilerplate))))))
