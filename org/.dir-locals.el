89c917721f8b317df97e6d3e6743b9b198094827
Jethro Kuan <jethrokuan95@gmail.com>
2020-02-28 16:00:33 +0800

    Fri 28 Feb 2020 04:00:34 PM +08

--- org/.dir-locals.el
+++ org/.dir-locals.el
@@ -1,5 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((org-mode . ((eval org-hugo-auto-export-mode)
-              (org-hugo-section . "zettels"))))
+((org-mode . ((eval jethro/conditional-hugo-enable))))
