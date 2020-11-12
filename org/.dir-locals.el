91bf5829f7e59e53adf38197d4068c61dd255ae0
Jethro Kuan <jethrokuan95@gmail.com>
2020-11-12 19:32:01 +0800

    Thu 12 Nov 2020 07:32:08 PM +08

--- org/.dir-locals.el
+++ org/.dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((org-mode . ((org-hugo-base-dir . "..")
-              (eval . (org-hugo-auto-export-mode)))))
+((org-mode . ((org-hugo-base-dir . "..")))
+ ("daily" . ((org-hugo-base-dir . nil))))
